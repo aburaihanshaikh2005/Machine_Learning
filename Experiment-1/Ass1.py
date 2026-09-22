16085b14a36b70dc65e651038a12579c1daf3344
Abu Raihan Shaikh <aburaihanshaikh2005@gmail.com>
2026-09-22 11:38:14 +0530

    Add files via upload

---
+++ Experiment-1/Ass1.py
@@ -0,0 +1,8 @@
+# Create a NumPy array containing the internal marks of 10 students.Calculate and  print the mean,median,standard deviation,maximum and minimum values.
+import numpy as np
+marks=np.array([60,56,78,96,46,77,76,86,63,61])
+print("Mean:",np.mean(marks))
+print("Median:",np.median(marks))
+print("Stand.Devia:",np.std(marks))
+print("Maximum:",np.max(marks))
+print("Minimum:",np.min(marks))
